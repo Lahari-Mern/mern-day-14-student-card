de652b2201160554023f9428472c1edf5cfb85a3
Lahari-Mern <laharinodagala@gmail.com>
2026-05-21 17:54:51 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <h1>Student Card Component</h1>
+      <h2>Name: Lahari Nodagala</h2>
+      <p>Course:Mern Stack</p>
+      <p>Age: 19</p>
+      <p>Every project is a step toward my goal in tech</p>
+    </div>
+  );
+}
+export default App;
